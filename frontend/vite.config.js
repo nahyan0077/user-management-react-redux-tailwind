e09b7b9fd35d4fff41adb613e0e9b81f55b6d7b3
nahyan0077 <nahyanmp4@gmail.com>
2024-07-19 19:13:15 +0530

    updated script , vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3002, // Change the port to your desired value
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 3000,
   },
 })
-
